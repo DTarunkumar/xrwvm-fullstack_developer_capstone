af924f8bd2aa31816b7b373cf9729d5cd59ffe15
Tarun Kumar <d.tarunkumarred@gmail.com>
2025-03-06 17:11:39 -0500

    python lint errors fix

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -67,7 +67,7 @@ def registration(request):
         # Create user in auth_user table
         user = User.objects.create_user(
             username=username, first_name=first_name,
-            last_name=last_name,password=password, email=email
+            last_name=last_name, password=password, email=email
         )
         # Login the user and redirect to list page
         login(request, user)
@@ -129,7 +129,7 @@ def get_dealer_reviews(request, dealer_id):
 # def add_review(request):
 # ...
 def add_review(request):
-    if (request.user.is_anonymous == False):
+    if request.user.is_anonymous is False:
         data = json.loads(request.body)
         try:
             post_review(data)
@@ -150,8 +150,8 @@ def get_cars(request):
     car_models = CarModel.objects.select_related('car_make')
     cars = []
     for car_model in car_models:
-        cars.append(
-            {"CarModel": car_model.name,
-            "CarMake": car_model.car_make.name}
-        )
+        cars.append({
+            "CarModel": car_model.name,
+            "CarMake": car_model.car_make.name
+        })
     return JsonResponse({"CarModels": cars})
